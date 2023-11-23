e43c870f44a81363637c9230698e5eba6c6c7691
ThomasTestaud <thomas.testaud@hotmail.fr>
2023-11-23 08:06:02 +0100

    ...

--- frontend2/src/components/user/DeleteAccount.vue
+++
@@ -1,8 +0,0 @@
-<template>
-    <div class="border border-red-600 rounded-lg lg:w-4/12 md:w-6/12 w-8/12 m-auto p-4 flex flex-col mt-4">
-        <h2 class="text-center pb-2">Delete your account</h2>
-        <hr>
-        <p class="text-center text-red-600">This action is irreversible.</p>
-        <button class="bg-red-600 hover:bg-red-500 rounded-lg my-4 text-white py-1">Delete my account</button>
-    </div>
-</template>
--- frontend2/src/views/eventsOnTime/DashboardsView.vue
+++
@@ -1,7 +0,0 @@
-<template>
-  <main>
-    <div class="border rounded-lg lg:w-4/12 md:w-6/12 w-8/12 m-auto p-4 flex flex-col mt-4 relative">
-      <p>This is the home page.</p>
-    </div>
-  </main>
-</template>
